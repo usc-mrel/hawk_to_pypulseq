d18ba3f9789f35ac0cd2d2180e06ad1055ae9c52
pkumar <prakashk@umich.edu>
2023-05-12 09:36:58 -0700

    added recon submodule

--- run_hawk_to_pulseq_convertor.py
+++ run_hawk_to_pulseq_convertor.py
@@ -12,24 +12,28 @@ seq, sys, repetitions, apd = hawk_to_pulseq(application_path)
 # write the sequence file
 seq.write("Sequences/Pulseq_Export/" + application_path.split("/")[-2] + '.seq')
 
-seq.plot()
+# seq.plot()
 
 # export k-space trajectory
 k_traj_adc, k_traj, t_excitation, t_refocusing, t_adc = seq.calculate_kspacePP()
 
+Nsample = int(k_traj_adc.shape[1]/repetitions)
 kx = k_traj_adc[0,:]
 ky = k_traj_adc[1,:]
+k_max = np.max(np.abs(kx + 1j * ky))
+k = (kx / k_max) + (1j * ky / k_max)
+# w = dcf_voronoi(k)
 
-# solve for density compensation weights. Assume it is the same each repetition for now.
-# this dcf computation is not really great, but it is a start. 
-# TODO: use voronoi density compensation instead.
+# calculate density compensation weights using Pipe and Menon's method
+# This isn't working so well... need to figure out why.
 Nsample = int(k_traj_adc.shape[1]/repetitions)
-w = mri.dcf.pipe_menon_dcf(k_traj_adc[:,0:Nsample+1].T)
+w = mri.dcf.pipe_menon_dcf(k_traj_adc.T)
+w = w[Nsample+1:2*Nsample+1]
 w = w / (np.max(w));
-w[w > 0.6] = 0.6;
+w[w > 0.4] = 0.4;
 w = w / np.max(w);   
 w[int(w.shape[0]*2/3):w.shape[0]] = 1
 
 apd.app_params['repetitions'] = repetitions
 
-savemat("/server/home/pkumar/pulseq_metadata/test.mat", {'kx': kx, 'ky': ky, 'w' : w, 'param': apd.app_params})
+savemat("ismrmrd-pypulseq-matlab-server/pulseq_metadata/" + application_path.split("/")[-2] + ".mat", {'kx': kx, 'ky': ky, 'w' : w, 'param': apd.app_params})
